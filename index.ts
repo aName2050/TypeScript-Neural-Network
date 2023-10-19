e0833945b6f78cd3cae9a3796a78b0eb61b69419
aName <shafaeebardia@gmail.com>
2023-10-19 17:28:47 +0000

    additional testing, debugging, and fixes

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
 import { data as trainingData } from "./Config/Data/dataset.json";
 /*
 Layer size needs to be fewer than the previous layer size, excluding input layer
-[inputLayerSize, ...hiddenLayerSizes, outputLayer]
+[inputLayerSize, ...hiddenLayerSizes < prevLayerSize, outputLayer < prevHiddenLayerSize]
 */
 import { NeuralNetwork } from "./Neural-Network/NeuralNetwork";
 console.log(`training data:`, trainingData);
 
 const neuralNetwork = new NeuralNetwork([2, 1, 1]);
-for (let i = 0; i < 2; i++) {
-  for (const data of trainingData) {
-    neuralNetwork.train(data.input, data.output);
+// for (let i = 0; i < 10000; i++) {
+//   for (const data of trainingData) {
+//     neuralNetwork.train(data.input, data.output);
 
-    console.log(`TRAINING... #${i} => IN: ${data.input} OUT: ${data.output}`);
-  }
-}
+//     console.log(`TRAINING... #${i} => IN: ${data.input} OUT: ${data.output}`);
+//   }
+// }
 const testInput = [1, 0];
 const output = neuralNetwork.forwardPropagation(testInput);
-console.log("Neural Network Output:", output); // Output: [1]
+console.log("Neural Network Output:", output); // Output: [1] : but it doesn't???
