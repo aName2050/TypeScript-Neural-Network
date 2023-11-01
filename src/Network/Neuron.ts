a381c88f6ce832ef7d218c80c87f29fabd69ed30
aName <68033001+aName2050@users.noreply.github.com>
2023-10-31 17:25:52 -0700

    formatter sync; some issue with it i need to fix

--- src/Network/Neuron.ts
+++ src/Network/Neuron.ts
@@ -1,33 +1,33 @@
-import { Calculus } from "../Math/Calculus";
-import { Random } from "../Math/Math";
-import { Activation } from "../Math/Activation";
+import { Calculus } from '../Math/Calculus';
+import { Random } from '../Math/Math';
+import { Activation } from '../Math/Activation';
 
 const calculus = new Calculus();
 
 export class Neuron {
-	public bias: number;
-	public weights: number[];
+    public bias: number;
+    public weights: number[];
 
-	/**
-	 *
-	 * @param inputs The number of inputs going into the neuron
-	 * @param inputNode Determine whether the current neuron is in the first layer
-	 */
-	constructor(inputs: number, inputNode?: boolean) {
-		if (inputNode) {
-			this.bias = 0;
-			this.weights = [0];
-		}
+    /**
+     *
+     * @param inputs The number of inputs going into the neuron
+     * @param inputNode Determine whether the current neuron is in the first layer
+     */
+    constructor(inputs: number, inputNode?: boolean) {
+        if (inputNode) {
+            this.bias = 0;
+            this.weights = [0];
+        }
 
-		this.bias = Random(1, -1);
-		this.weights = Array.from({ length: inputs }, () => Random(-1, 1));
-	}
+        this.bias = Random(1, -1);
+        this.weights = Array.from({ length: inputs }, () => Random(-1, 1));
+    }
 
-	public CalculateNeuronActivation(neuronInputs: number[]): number {
-		let z: number = calculus.WeightedSumCalculation(
-			neuronInputs,
-			this.weights
-		);
-		return z + this.bias;
-	}
+    public CalculateNeuronActivation(neuronInputs: number[]): number {
+        let z: number = calculus.WeightedSumCalculation(
+            neuronInputs,
+            this.weights
+        );
+        return z + this.bias;
+    }
 }
