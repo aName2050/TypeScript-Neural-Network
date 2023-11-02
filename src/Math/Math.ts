31880133639cb418d4099db64e424984993a355b
aName <68033001+aName2050@users.noreply.github.com>
2023-11-01 19:44:34 -0700

    fixed random number gen

--- src/Math/Math.ts
+++ src/Math/Math.ts
@@ -5,7 +5,7 @@
  * @returns The random number
  */
 export function Random(max: number, min: number): number {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.round(Math.random() * (max - min + 1) + min);
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.random() * (max - min + 1) + min;
 }
