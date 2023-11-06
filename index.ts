80943fb502e6acfb22b054b6a50284caa317ee3b
aName <shafaeebardia@gmail.com>
2023-11-06 20:30:10 +0000

    added more logs for progress monitoring

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { data as trainingData } from "./Config/Data/dataset.json"; // XOR training dataset
+import { data as trainingData } from "./Config/Data/dataset.json"; // OR training dataset
 import { learnCycles, learnRate } from "./Config/config.json"; // training config
 
 import { NeuralNetwork } from "./src/Network/NeuralNetwork";
@@ -7,10 +7,15 @@ import chalk from "chalk";
 
 const NN: NeuralNetwork = new NeuralNetwork(2, 1, 1);
 // Pre-training test
-console.log(
-	`${chalk.bold.bgCyan("PRE-TRAINING TEST...............................")}`
-);
-NN.forwardPropagation([0, 0]); // Expect horribly wrong (or right) answer (target 0)
+let preTrainTestResults: number[][] = [];
+console.log(chalk.bgKeyword("orange").bold("Input: [0, 0] Expect: ~ 0 "));
+preTrainTestResults.push(NN.forwardPropagation([0, 0])); // Expect horribly wrong (or right) answer (target 0)
+console.log(chalk.bgKeyword("orange").bold("Input: [0, 1] Expect: ~ 1 "));
+preTrainTestResults.push(NN.forwardPropagation([0, 1])); // Expect horribly wrong (or right) answer (target 1)
+console.log(chalk.bgKeyword("orange").bold("Input: [1, 0] Expect: ~ 1 "));
+preTrainTestResults.push(NN.forwardPropagation([1, 0])); // Expect horribly wrong (or right) answer (target 1)
+console.log(chalk.bgKeyword("orange").bold("Input: [1, 1] Expect: ~ 1 "));
+preTrainTestResults.push(NN.forwardPropagation([1, 1])); // Expect horribly wrong (or right) answer (target 1)
 
 // Train network
 console.log(
@@ -45,4 +50,27 @@ console.log(`TrainingData.length: ${trainingData.length}`);
 console.log(
 	`${chalk.bold.bgCyan("POST-TRAINING TEST...............................")}`
 );
+console.log(chalk.bgKeyword("orange").bold("Input: [0, 0] Expect: ~ 0 "));
 NN.forwardPropagation([0, 0]); // Expect a somewhat right answer (target 0)
+console.log(chalk.bgKeyword("orange").bold("Input: [0, 1] Expect: ~ 1 "));
+NN.forwardPropagation([0, 1]); // Expect a somewhat right answer (target 1)
+console.log(chalk.bgKeyword("orange").bold("Input: [1, 0] Expect: ~ 1 "));
+NN.forwardPropagation([1, 0]); // Expect a somewhat right answer (target 1)
+console.log(chalk.bgKeyword("orange").bold("Input: [1, 1] Expect: ~ 1 "));
+NN.forwardPropagation([1, 1]); // Expect a somewhat right answer (target 1)
+
+// Pre-training test
+console.log(
+	`${chalk.bold.bgCyan("PRE-TRAINING TEST...............................")}`
+);
+const displayTexts: string[] = [
+	"Input: [0, 0] Expect: ~ 0",
+	"Input: [0, 1] Expect: ~ 1",
+	"Input: [1, 0] Expect: ~ 1",
+	"Input: [1, 1] Expect: ~ 1",
+];
+preTrainTestResults.forEach((result, i) => {
+	console.log(
+		chalk.bgKeyword("pink").bold(`${displayTexts[i]} Result: ${result} `)
+	);
+});
